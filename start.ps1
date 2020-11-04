7cc35668a2fbf1d317946f3ff5e740681f26c995
Jim Speir <jimbo80982@gmail.com>
2020-11-04 15:22:27 +0000

    Fixing slashes and command

--- start.ps1
+++ start.ps1
@@ -1,4 +1,4 @@
 echo $Env:RUNNERURL
 echo $Env:RUNNERTOKEN
-./actions-runner/config.md --url $Env:RUNNERURL --token $Env:RUNNERTOKEN
-./actions-runner/run.cmd
+.\actions-runner\config.cmd --url $Env:RUNNERURL --token $Env:RUNNERTOKEN
+.\actions-runner\run.cmd
